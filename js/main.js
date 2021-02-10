c38f6b2e0aceed2b02351c590d74a55978b66e70
Omar Ashraf <omar_ashraf_93@hotmail.com>
2021-02-10 19:25:13 +0300

    Added jQuery, Bootstrap & Created The Header(nav)

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let toggleBtn = document.getElementById('toggle-btn');
+let mobNav = document.getElementById('mob-nav');
+
+toggleBtn.addEventListener("click",function(){
+    mobNav.classList.toggle('show');
+})
+
