f9e9a659cd58822d8cf3759c850de8ba757c2a29
Omar Ashraf <omar_ashraf_93@hotmail.com>
2021-02-11 13:51:51 +0300

    Final Touches for Nav & Hero Section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
-let toggleBtn = document.getElementById('toggle-btn');
-let mobNav = document.getElementById('mob-nav');
+// --------------------------------------------------------------------------------- Mobile Nav Show & Hide ******
+let toggleBtn = document.getElementById("toggle-btn");
+let mobNav = document.getElementById("mob-nav");
+let sections = document.getElementsByTagName("section");
+let navLinks = document.getElementsByClassName("nav-link");
 
-toggleBtn.addEventListener("click",function(){
-    mobNav.classList.toggle('show');
-})
+// Add/remove .show class which show/hide the Mobile Nav
+toggleBtn.addEventListener("click", function () {
+  mobNav.classList.toggle("show");
+});
 
+// Auto Close Mobile Nav When Link is Clickec
+for (i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", function () {
+    mobNav.classList.toggle("show");
+  });
+}
+
+// --------------------------------------------------------------------------------- Desktop Nav Have Border Bottom After 100px Scroll ******
+let header = document.getElementsByTagName("header");
+
+// if Scrolled Area is greater than 300px the Border will appear, else will not appear
+document.addEventListener("scroll", function () {
+  if (scrollY > 300) {
+    header[0].style.borderBottom = "2px solid #CCC";
+  } else if (scrollY < 300) {
+    header[0].style.borderBottom = "none";
+  }
+});
